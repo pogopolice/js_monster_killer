61402eb3c61ce62f36e14d26b1abd369afa08974
pogopolice <pogopolice@gmail.com>
2022-01-13 21:03:43 -0500

    use a for in loop in the print log handler function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -235,10 +235,12 @@ function printLogHandler() {
   // }
   let i = 0;
   for (const logEntry of battleLog) {
-    console.log(logEntry);
-    console.log(i);
+    console.log(`#${i}`);
+    for (const key in logEntry) {
+      console.log(`${key} => ${logEntry[key]}`);
+    }
     i++;
-  };
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
