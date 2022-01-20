f04c66a3cbd1c9b433b4d0a0048206044761b6a0
pogopolice <pogopolice@gmail.com>
2022-01-20 08:51:21 -0500

    use try to test the user input and impose a default if there is an error

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -24,7 +24,16 @@ function getMaxLifeValues () {
   return parsedValue;
 }
 
-let chosenMaxLife = getMaxLifeValues();
+let chosenMaxLife;
+
+try {
+  chosenMaxLife = getMaxLifeValues();
+} catch(error) {
+  console.log(error);
+  chosenMaxLife = 100;
+  alert('You entered something wrong, default value of 100 was used.');
+}
+
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
